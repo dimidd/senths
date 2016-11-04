886fdf401e610906c93a453f7b8973419324f9d9
Dimid Duchovny <dimidd@gmail.com>
2016-11-04 11:13:18 +0200

    main: add interactive mode
    Signed-off-by: Dimid Duchovny <dimidd@gmail.com>

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 import NBTrain (tsvToStats, tsvToModel)
 import NB (testUtterance, Model)
-import System.IO (hSetBuffering, stdout, BufferMode (NoBuffering) )
+import System.IO (hSetBuffering, stdout, BufferMode (NoBuffering), isEOF)
 
 endToEnd :: IO (Int, Int, Float)
 endToEnd = tsvToStats "../data/train.tsv" "../data/test.tsv"
@@ -16,12 +16,20 @@ loop :: Model -> IO ()
 loop model = do
     putStrLn "Enter a tech/biz headline to classify. Empty string or Cntrl-C to exit"
     putStr "> "
-    sent <- getLine
-    case sent of
-      "" -> putStrLn "Bye."
-      _ -> do
-            let klass = testUtterance model sent
-            putStrLn $ case klass of
-              "t" -> "tech"
-              _ ->  "biz"
-            loop model
+    done <- isEOF
+    if done then
+        putStrLn "Bye."
+    else
+        do
+            sent <- getLine
+            case sent of
+              "" -> putStrLn "Bye."
+              _ -> do
+                    let klass = testUtterance model sent
+                    printClass sent $ case klass of
+                      "t" -> "tech"
+                      _ ->  "biz"
+                    loop model
+
+printClass :: String -> String -> IO ()
+printClass sent c = putStrLn $ " I believe '" ++ sent ++ "' belongs to: " ++ c 
